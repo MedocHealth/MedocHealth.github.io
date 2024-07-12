1c039ffd4ce45ce644e279e56253038e8dcaa56a
Subham Kumar Sharma <subhamrelax45@gmail.com>
2024-07-12 12:29:49 +0530

    blog url

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -593,7 +593,7 @@ function HomePage() {
         {/* <ParallaxLayer offset={18} style={{ backgroundColor: "#EEEEEE" }}> */}
 
 
-        <ParallaxLayer offset={18} style={{ backgroundColor: "#EEEEEE" }}>
+        <ParallaxLayer offset={19} style={{ backgroundColor: "#EEEEEE" }}>
 
           <div className={styles.contactus}>
             <h1>Looking for Support?</h1>
@@ -641,7 +641,7 @@ function HomePage() {
             </form>
           </div>
         </ParallaxLayer>
-        <ParallaxLayer offset={19} style={{ backgroundColor: "#EEEEEE" }}>
+        <ParallaxLayer offset={20} style={{ backgroundColor: "#EEEEEE" }}>
           <div className={styles.footer}>
             <div className={styles.footerflex}>
               <div className={styles.footercontb}>
