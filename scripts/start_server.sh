b7472d69e0885e62e0db6e475d2204468c549f3e
Akshat Choudhary <114065418+TheDedster@users.noreply.github.com>
2024-08-07 23:54:57 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-service httpd start
+
+# Remove old build files
+sudo rm -rf /var/www/html/*
+
+# Copy new build files to Nginx root directory
+sudo cp -r /home/ubuntu/MedocHealth.github.io/build/* /var/www/html/
+
+# Restart Nginx to apply changes
+sudo systemctl restart nginx
